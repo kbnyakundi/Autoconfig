2f9f13ea41fd21c5458a0bf32dfb247a5f87de5e
bosire <nyakundi@hotmail.co.uk>
2019-11-17 09:13:41 +0000

    basic app working

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,18 +3,36 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
+import M from 'materialize-css';
+import {Button, Icon} from 'react-materialize';
 
 type Props = {};
 
 export default class Home extends Component<Props> {
   props: Props;
 
+  componentDidMount(){
+  	M.AutoInit();
+  }
+
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-      </div>
+    	<div className="container center-align">
+	      <div className="row" id="title-div">
+	        <h1>A u t o C o n f i g</h1>
+	        <h5>Because who ever enjoyed setting up their development environment!</h5>
+	       </div>
+	        <div className="row">
+	        	<a class="waves-effect waves-light btn-large">Create React-Flask App</a>
+	        </div>
+	        <div className="row">
+	        	<a class="waves-effect waves-light btn-large">Create React-block App</a>
+	        </div>
+	        <div className="row">
+	        	<a class="waves-effect waves-light btn-large">Create bosire App</a>
+	        </div>
+	        <Link to={routes.COUNTER}>A coders best friend.</Link>
+      	</div>
     );
   }
 }
